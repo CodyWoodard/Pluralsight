f54d3214cc72abe38bd713070f1d91970a96d10b
CodyWoodard <you@example.com>
2024-09-03 12:25:51 -0500

    Functools & Wraps

--- decorators.py
+++ decorators.py
@@ -1,15 +1,27 @@
+from functools import wraps
+
 
 def email_decorator(func):
-    def wrapper():
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # args = ("desk",)
+        # kwargs = {}
         print("Dear interns, ")
-        func()
+        func(*args, **kwargs)
+        # func("desk")
         print("Best regards, ")
         print("your new boss")
     return wrapper
 
 @email_decorator
-def greeting_message():
-    print("Welcome to your new job!")
-    
-greeting_message()
+def greeting_message(x):
+    """ Welcome to Keep message
 
+    Args:
+        x (any): desk
+    """
+    print(f"Welcome to your new {x}")
+    
+greeting_message("desk")
+print(greeting_message.__name__)
+print(greeting_message.__doc__)
