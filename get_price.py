69733c10b8173ed6879a3a224623aac083770fd8
CodyWoodard <you@example.com>
2024-10-10 13:21:55 -0500

    More changes to the sql query

--- get_price.py
+++ get_price.py
@@ -17,16 +17,10 @@ create_table_query = """
 
 investment = ("bitcoin", "usd", True, 1.0, datetime.datetime.now())
 
-cursor.execute(
-    "INSERT INTO investments VALUES (?, ?, ?, ?, ?);",
-    investment)
-database.commit()
 
-result = cursor.execute("SELECT * FROM investments;")
 
-all_rows = result.fetchall()
 
-first_or_only_row = result.fetchone()
+
 
 @click.group()
 def cli():
@@ -49,4 +43,12 @@ if __name__ == "__main__":
     database = sqlite3.connect("portfolio.db")
     cursor = database.cursor()
     cursor.execute()
+    cursor.execute(
+        "INSERT INTO investments VALUES (?, ?, ?, ?, ?);",
+        investment)
+    database.commit()
+
+    result = cursor.execute("SELECT * FROM investments;")
+    all_rows = result.fetchall()
+    first_or_only_row = result.fetchone()
     cli()
